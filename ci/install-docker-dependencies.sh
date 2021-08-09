ba5ce0d0b45e2fef93a93c6e9f15652679c7958a
Carlo Marcelo Arenas Belón <carenas@gmail.com>
2021-08-08 18:38:33 -0700

    ci: run a pedantic build as part of the GitHub workflow
    similar to the recently added sparse task, it is nice to know as early
    as possible.
    
    add a dockerized build using fedora (that usually has the latest gcc)
    to be ahead of the curve and avoid older ISO C issues at the same time.
    
    Signed-off-by: Carlo Marcelo Arenas Belón <carenas@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- ci/install-docker-dependencies.sh
+++ ci/install-docker-dependencies.sh
@@ -15,4 +15,10 @@ linux-musl)
 	apk add --update build-base curl-dev openssl-dev expat-dev gettext \
 		pcre2-dev python3 musl-libintl perl-utils ncurses >/dev/null
 	;;
+pedantic)
+	dnf -yq update >/dev/null &&
+	dnf -yq install gcc make findutils diffutils perl-interpreter \
+		gettext git zlib-devel expat-devel openssl-devel \
+		curl-devel pcre2-devel >/dev/null
+	;;
 esac
