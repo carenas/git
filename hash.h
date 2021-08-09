144069e29907f3fd38283d21c2a28ff88a9ed535
Carlo Marcelo Arenas Belón <carenas@gmail.com>
2021-08-08 20:45:56 -0700

    build: fixes for SANITIZE=undefined (WIP)
    from instructions/code provided by René in :
    
      https://lore.kernel.org/git/20210807224957.GA5068@dcvr/
    
    tested with Xcode in macOS 11.5.1 (x86_64)

--- hash.h
+++ hash.h
@@ -115,7 +115,7 @@ static inline void git_SHA256_Clone(git_SHA256_CTX *dst, const git_SHA256_CTX *s
 
 struct object_id {
 	unsigned char hash[GIT_MAX_RAWSZ];
-	int algo;
+	uint8_t algo;
 };
 
 /* A suitably aligned type for stack allocations of hash contexts. */
