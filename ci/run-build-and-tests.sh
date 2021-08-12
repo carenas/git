b949c6ef5809b82471b4a36fbdf63437fb614153
Carlo Marcelo Arenas Belón <carenas@gmail.com>
2021-08-11 22:43:21 -0700

    DONOTMERGE: ci test

--- ci/run-build-and-tests.sh
+++ ci/run-build-and-tests.sh
@@ -7,7 +7,10 @@
 
 case "$CI_OS_NAME" in
 windows*) cmd //c mklink //j t\\.prove "$(cygpath -aw "$cache_dir/.prove")";;
-*) ln -s "$cache_dir/.prove" t/.prove;;
+*)
+	ln -s "$cache_dir/.prove" t/.prove
+	export SANITIZE=undefined
+	;;
 esac
 
 make
